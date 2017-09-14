51b5376c83d85cccaf317d1035e562ad9c8146e7
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2017-09-14 16:15:52 +0600

    2017-09-14
    2017-09-14

---
+++ lab.themeapps/lab.themeapps/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab.themeapps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sample1()
+        {
+            return View();
+        }
+
+        public ActionResult Sample2()
+        {
+            return View();
+        }
+
+        public ActionResult Sample3()
+        {
+            return View();
+        }
+
+        public ActionResult Sample4()
+        {
+            return View();
+        }
+
+        public ActionResult Sample5()
+        {
+            return View();
+        }
+
+        public ActionResult Sample6()
+        {
+            return View();
+        }
+
+        public ActionResult Sample7()
+        {
+            return View();
+        }
+
+        public ActionResult Sample8()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab.themeapps/lab.themeapps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab.themeapps.Startup))]
+namespace lab.themeapps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
